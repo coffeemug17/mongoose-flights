a232c4cdd835f3b227d5c3da62e3c1c6c98ccbe9
coffeemug17 <mitulpandey2@gmail.com>
2023-02-05 19:57:48 -0800

    Connection to database working

--- config/database.js
+++ config/database.js
@@ -1,1 +1,11 @@
-re
+const mongoose = require('mongoose');
+
+//connecting it using the credentials in the.env file
+mongoose.connect(process.env.DATABASE_URL);
+
+//shortcut to the database connection object
+const db = mongoose.connection;
+
+db.on('connected', function() {
+    console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
+});
